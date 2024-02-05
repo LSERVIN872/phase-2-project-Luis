b34c1992c21e7f8cd811ca420eb561dc7f682d6e
luis <servinluis21@yahoo.com>
2024-02-05 15:36:01 -0500

    Navbar CSS added

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,8 @@ function NavBar() {
     return (
       <nav className="navbar">
         <NavLink to="/" >Home</NavLink>
-        <NavLink to=</NavLink>
-        <NavLink to=/NavLink>
+        <NavLink to="/gifts">Gifts</NavLink>
+        <NavLink to= "schedule">Schedule</NavLink>
       </nav>
     );
   }
